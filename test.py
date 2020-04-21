545d621bbb39bbbabe913a6c850df3b684500b23
Atekawere <jacintawere2@gmail.com>
2020-04-22 00:04:52 +0300

    make changes

--- test.py
+++ test.py
@@ -13,3 +13,13 @@ class TestUser(unittest.TestCase):
         Function to create a user account before each test
         '''
         self.new_user = User('Jacinta', 'At\'ek\'a' 'pswd540')
+
+        def test__init__(self):
+            '''
+            Test to if check the initialization/creation of user instances is properly done
+            '''
+            self.assertEqual(self.new_user.first_name, 'Jacinta')
+            self.assertEqual(self.new_user.last-name, 'At\'ek\'a')
+            self.assertEqual(self.new_user.password, 'pswd540')
+
+            
