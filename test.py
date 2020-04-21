62b2f9c5772fb6755cb90e1b5cc5913b538e0b76
Atekawere <jacintawere2@gmail.com>
2020-04-21 23:53:28 +0300

    made a set up on test.py

--- test.py
+++ test.py
@@ -1,3 +1,15 @@
-import unnitest
+import unittest
 import pyperclip
 from user import User, Credential
+
+class TestUser(unittest.TestCase):
+    '''
+    Test class that defines test cases for the user behaviors.
+    Args:
+        unittest.TestCase: helps in creating test cases
+    '''
+    def SetUp(self):
+        '''
+        Function to create a user account before each test
+        '''
+        self.new_user = User('Jacinta', 'At\'ek\'a' 'pswd540')
