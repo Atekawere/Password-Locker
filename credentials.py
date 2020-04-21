01a5ac51b133f348aac3b091b843509563b18ea3
Atekawere <jacintawere2@gmail.com>
2020-04-22 02:03:32 +0300

    added user verification

--- credentials.py
+++ credentials.py
@@ -6,3 +6,19 @@ def create_user(fname,lname,password):
     '''
     Function to create a new user account
     '''
+    new_user = User(fname,lname,password)
+    return new_user
+
+def save_user(user):
+    '''
+    Function to save a new user account
+    '''
+    User.save_user(user)
+
+
+def verify_user(first_name,password):
+    '''
+    Function that verifies the existance of the user before creating credentials
+    '''
+    checking_user = Credential.check_user(first_name,password)
+    
